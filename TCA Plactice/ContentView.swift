99af7d4ad5cdd507c58153a6bf49fe42b0e68de7
Kim JaeHyung <you3192001@icloud.com>
2024-06-03 12:18:20 +0900

    Initial Commit

---
+++ TCA Plactice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA Plactice
+//
+//  Created by Jae hyung Kim on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA Plactice/TCA_PlacticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PlacticeApp.swift
+//  TCA Plactice
+//
+//  Created by Jae hyung Kim on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PlacticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
